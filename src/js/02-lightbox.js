05b95eab656efe8b7487ca1b0b9e275bfb7d7fda
MOLO4KOO <94447118+MOLO4KOO@users.noreply.github.com>
2022-07-20 22:33:02 +0300

    goit-js-hw07

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,39 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryEl.addEventListener("click", onGalleryClick);
+function createImgCardsMarkup(galleryItems){
+    
+    return galleryItems.map(({preview, original, description}) => {
+        return   `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+    }).join('');
+};
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+    if(!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    openLightbox();
+};
+function openLightbox() {
+    let lightbox = new SimpleLightbox('.gallery a',
+        {
+            captionSelector: 'img',
+            captionType: 'attr',
+            captionsData: 'alt',
+            captionPosition: 'top',
+            captionDelay: 900,
+            enableKeyboard: true,
+            maxZoom: 0,
+            fadeSpeed: 700,
+        });
+    lightbox.open();
+};
